b9dd385acf26bbcdfb241ba26911c9116cdea2fe
MohammadJewelRana <js.rana0326@gmail.com>
2026-03-02 11:54:42 +0600

    send response file created and otimized codebase in controller

--- feedback-backend/src/app/module/feedback/feedback.controller.ts
+++ feedback-backend/src/app/module/feedback/feedback.controller.ts
@@ -1,12 +1,15 @@
 import { catchAsync } from "../../utils/catchAsync";
+import sendResponse from "../../utils/sendResponse";
 import { FeedbackServices } from "./feedback.service";
-import { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
 
 // create feedback
 const createFeedback = catchAsync(async (req, res) => {
   const payload = req.body;
   const result = await FeedbackServices.createFeedbackIntoDB(payload);
-  res.status(201).json({
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
     success: true,
     message: "Feedback created successfully",
     data: result,
@@ -16,7 +19,9 @@ const createFeedback = catchAsync(async (req, res) => {
 //get all feedback
 const getAllFeedback = catchAsync(async (req, res) => {
   const result = await FeedbackServices.getAllFeedback(req.query);
-  res.status(200).json({
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
     success: true,
     message: "Feedback fetched successfully",
     data: result,
@@ -28,7 +33,8 @@ const getSingleFeedback = catchAsync(async (req, res) => {
   const id = req.params.id as string;
   const result = await FeedbackServices.getSingleFeedback(id);
 
-  res.status(200).json({
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
     success: true,
     message: "Single Feedback fetched successfully",
     data: result,
@@ -40,7 +46,8 @@ const deleteSingleFeedback = catchAsync(async (req, res) => {
   const id = req.params.id as string;
   const result = await FeedbackServices.deleteSingleFeedback(id);
 
-  res.status(200).json({
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
     success: true,
     message: "Single Feedback deleted successfully",
     data: result,
@@ -53,7 +60,8 @@ const updateSingleFeedback = catchAsync(async (req, res) => {
   const payload = req.body;
   const result = await FeedbackServices.updateSingleFeedback(id, payload);
 
-  res.status(200).json({
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
     success: true,
     message: "Single Feedback updated successfully",
     data: result,
