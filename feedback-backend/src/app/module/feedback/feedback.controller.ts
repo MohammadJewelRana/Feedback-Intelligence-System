aa55af809daaf22b206a9b1c6289c216880f9126
MohammadJewelRana <js.rana0326@gmail.com>
2026-03-02 11:49:48 +0600

    catchasync function added and implementation in controller to simplified codebase

--- feedback-backend/src/app/module/feedback/feedback.controller.ts
+++ feedback-backend/src/app/module/feedback/feedback.controller.ts
@@ -1,109 +1,64 @@
+import { catchAsync } from "../../utils/catchAsync";
 import { FeedbackServices } from "./feedback.service";
 import { NextFunction, Request, Response } from "express";
 
 // create feedback
-const createFeedback = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const payload = req.body;
-    const result = await FeedbackServices.createFeedbackIntoDB(payload);
-    res.status(201).json({
-      success: true,
-      message: "Feedback created successfully",
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-    console.log(error);
-  }
-};
+const createFeedback = catchAsync(async (req, res) => {
+  const payload = req.body;
+  const result = await FeedbackServices.createFeedbackIntoDB(payload);
+  res.status(201).json({
+    success: true,
+    message: "Feedback created successfully",
+    data: result,
+  });
+});
 
 //get all feedback
-const getAllFeedback = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const result = await FeedbackServices.getAllFeedback(req.query);
-
-    res.status(200).json({
-      success: true,
-      message: "Feedback fetched successfully",
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-    console.log(error);
-  }
-};
+const getAllFeedback = catchAsync(async (req, res) => {
+  const result = await FeedbackServices.getAllFeedback(req.query);
+  res.status(200).json({
+    success: true,
+    message: "Feedback fetched successfully",
+    data: result,
+  });
+});
 
 //get single  feedback
-const getSingleFeedback = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const id = req.params.id as string;
-    const result = await FeedbackServices.getSingleFeedback(id);
+const getSingleFeedback = catchAsync(async (req, res) => {
+  const id = req.params.id as string;
+  const result = await FeedbackServices.getSingleFeedback(id);
 
-    res.status(200).json({
-      success: true,
-      message: "Single Feedback fetched successfully",
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-    console.log(error);
-  }
-};
+  res.status(200).json({
+    success: true,
+    message: "Single Feedback fetched successfully",
+    data: result,
+  });
+});
 
 //delete single  feedback
-const deleteSingleFeedback = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const id = req.params.id as string;
-    const result = await FeedbackServices.deleteSingleFeedback(id);
+const deleteSingleFeedback = catchAsync(async (req, res) => {
+  const id = req.params.id as string;
+  const result = await FeedbackServices.deleteSingleFeedback(id);
 
-    res.status(200).json({
-      success: true,
-      message: "Single Feedback deleted successfully",
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-    console.log(error);
-  }
-};
+  res.status(200).json({
+    success: true,
+    message: "Single Feedback deleted successfully",
+    data: result,
+  });
+});
 
 //update single  feedback
-const updateSingleFeedback = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const id = req.params.id as string;
-    const payload = req.body;
-    const result = await FeedbackServices.updateSingleFeedback(id, payload);
+const updateSingleFeedback = catchAsync(async (req, res) => {
+  const id = req.params.id as string;
+  const payload = req.body;
+  const result = await FeedbackServices.updateSingleFeedback(id, payload);
 
-    res.status(200).json({
-      success: true,
-      message: "Single Feedback updated successfully",
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-    console.log(error);
-  }
-};
+  res.status(200).json({
+    success: true,
+    message: "Single Feedback updated successfully",
+    data: result,
+  });
+});
 
 export const FeedbackController = {
   createFeedback,
