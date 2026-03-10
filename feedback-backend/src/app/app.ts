93503e468d7e3f19ebdefe28eb769564dd62e89e
MohammadJewelRana <js.rana0326@gmail.com>
2026-03-10 16:20:47 +0600

    backend cors policy updated

--- feedback-backend/src/app/app.ts
+++ feedback-backend/src/app/app.ts
@@ -1,4 +1,4 @@
-import express, { Application, NextFunction, Request, Response } from "express";
+import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import notFound from "./middleware/notFound";
 import router from "./routes";
@@ -6,26 +6,32 @@ import globalErrorHandler from "./middleware/globalErrorHandler";
 
 const app: Application = express();
 
-//Middlewares
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:3001",
+  "https://feedback-frontend.vercel.app",
+];
+
 const corsOptions = {
-  origin: [
-    "http://localhost:3000", // Localhost for development
-    "http://localhost:3001", // Localhost for development
-  ],
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  origin: function (origin: any, callback: any) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS not allowed"));
+    }
+  },
   credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization"],
 };
 
 app.use(cors(corsOptions));
-// app.options('*', cors(corsOptions));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", router);
 
 // base route
-app.get("/api", (_req: Request, res: Response) => {
+app.get("/", (_req: Request, res: Response) => {
   res.status(200).json({
     success: true,
     message: "Feedback Backend Server is running 🚀",
@@ -33,6 +39,6 @@ app.get("/api", (_req: Request, res: Response) => {
 });
 
 app.use(globalErrorHandler);
-app.use(notFound); //not found route
+app.use(notFound);
 
 export default app;
